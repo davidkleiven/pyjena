ecea78cf812065beeec1819f78f5d552aa725015
David Kleiven <davidkleiven446@gmail.com>
2024-08-06 21:35:54 +0200

    test: add test that loads data from two rdf files

--- kotlin/src/test/kotlin/TestTdbContainer.kt
+++ kotlin/src/test/kotlin/TestTdbContainer.kt
@@ -1,5 +1,7 @@
 package com.github.davidkleiven.pyjena.test
 import com.github.davidkleiven.pyjena.test.tutils.InitializedTdbContainerFactory
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -13,10 +15,33 @@ internal class TestTdbContainer {
     }
 
     @Test
-    fun `test triple extraction`() {
+    fun `test example1 has 4 triples`() {
         val container = InitializedTdbContainerFactory().example1()
-        val res = container.execSelect("select * where {?s <http://example.org/stuff/1.0/fullName> ?name}")
-        assertEquals(res.result.bindings.size, 1)
-        assertEquals("Dave Beckett", res.result.bindings[0]["name"]!!.value)
+        val res = container.execSelect("select * where {?s ?p ?o}")
+        assertEquals(4, res.result.bindings.size)
+    }
+
+    @TestFactory
+    fun `test triple extraction`() =
+        listOf(InitializedTdbContainerFactory().example1(), InitializedTdbContainerFactory().example1AndExample2()).map {
+            DynamicTest.dynamicTest("Test triple extraction") {
+                val res = it.execSelect("select * where {?s <http://example.org/stuff/1.0/fullName> ?name}")
+                assertEquals(res.result.bindings.size, 1)
+                assertEquals("Dave Beckett", res.result.bindings[0]["name"]!!.value)
+            }
+        }
+
+    @Test
+    fun `test example2 count is 1`() {
+        val container = InitializedTdbContainerFactory().example2()
+        val res = container.execSelect("select * where {?s ?p ?o}")
+        assertEquals(1, res.result.bindings.size)
+    }
+
+    @Test
+    fun `test example1 and example2 count is 5`() {
+        val container = InitializedTdbContainerFactory().example1AndExample2()
+        val res = container.execSelect("select * where {?s ?p ?o}")
+        assertEquals(5, res.result.bindings.size)
     }
 }
