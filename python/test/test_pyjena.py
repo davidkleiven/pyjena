6576ea28beefeb3fe79a9fc1f74fdb647097f0b4
David Kleiven <david.kleiven@statnett.no>
2024-08-09 21:21:26 +0200

    test: add client to be used in tests

--- python/test/test_pyjena.py
+++ python/test/test_pyjena.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 from pyjena.tdb_container_wrapper import TdbContainerWrapper
+from pyjena.gateway_client import GatewayClient
 
 TEST_RESOURCES = Path(__file__).parent.parent.parent / "kotlin/src/test/resources"
 
 
-def test_load_example1():
+def test_load_example1(client: GatewayClient):
     data_file = TEST_RESOURCES / "example1.rdf"
-    wrapper = TdbContainerWrapper()
+    wrapper = TdbContainerWrapper(client)
     wrapper.load([data_file])
     res = wrapper.exec_select("select (count(*) as ?cnt) where {?s ?p ?o}")
 
